880e292832af483c05728d465c1890720096649e
[Azl369] <[2992329876@qq.com]>
2022-10-17 15:45:54 +0800

    点亮led灯

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 
-void setup() {
+#define ledpin PC0
+int delay_time = 500;
+
+void setup()
+{
   // put your setup code here, to run once:
+  pinMode(ledpin, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  digitalWrite(ledpin, HIGH);
+  delay(delay_time);
+  digitalWrite(ledpin, LOW);
+  delay(delay_time);
 }
